cf0686ddec3540d160274e2b0474353f88d18805
Mayank Jain <mayank13@gmail.com>
2016-06-13 16:48:48 +0530

    Fibonacci formatting

--- Recursion/Fibonacci.js
+++ Recursion/Fibonacci.js
@@ -1,7 +1,7 @@
-function Fibonacci(n){
-	if( n === 0 || n === 1 ){
+function Fibonacci(n) {
+	if (n === 0 || n === 1) {
 		return n;
-	}else{
-		return Fibonacci(n-1) + Fibonacci(n-2);
+	} else {
+		return Fibonacci(n - 1) + Fibonacci(n - 2);
 	}
 }
