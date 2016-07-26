e160f41d19847f98e09fbb1418087733466516de
Mayank Jain <mayank13@gmail.com>
2016-07-26 17:20:07 +0530

    Merge Sort Optimised

---
+++ Sorts/MergeSort-opt.js
@@ -0,0 +1,60 @@
+var fs = require('fs');
+fs.readFile('./data.json', 'utf-8', function(err, data) {
+	var inputArray = data.split('\r\n').map(Number);
+	//console.log(inputArray);
+	mergerSort(inputArray);
+});
+
+function mergerSort(arr) {
+	var aux = [];
+	sort(arr, aux, 0, arr.length - 1);
+	console.log(arr);
+}
+
+function sort(arr, aux, lo, hi) {
+	if (hi <= lo) return;
+	var mid = Math.floor((hi + lo) / 2);
+	sort(arr, aux, lo, mid);
+	sort(arr, aux, mid + 1, hi);
+	merge(arr, aux, lo, mid, hi);
+}
+
+function merge(arr, aux, lo, mid, hi) {
+
+	//copying
+	for (var i = lo; i <= hi; i++) {
+		aux[i] = arr[i];
+	}
+	var i = lo;
+	var j = mid + 1;
+	var k = 0;
+
+
+	// This looping is not easily readable, need to refactor
+	while (i < (mid - lo + 1) && j < (hi + 1)) {
+		if (aux[j] < aux[i]) {
+			arr[k] = aux[j];
+			j++;
+			k++;
+		} else {
+			arr[k] = aux[i];
+			i++;
+			k++;
+		}
+	}
+
+	while (i < (mid - lo + 1)) {
+		arr[k] = aux[i];
+		i++;
+		k++;
+	}
+	while (j < (hi - mid + 1)) {
+		arr[k] = aux[j];
+		j++;
+		k++;
+	}
+	//console.log(aux);
+}
+//Tests
+console.log("Test merge ::");
+merge([4, 5, 6, 1, 2, 3], [], 0, 2, 5);
