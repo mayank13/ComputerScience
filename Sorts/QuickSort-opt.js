9d651cca7ca2354e39fd50edb2d873298fe2b7e3
Mayank Jain <mayank13@gmail.com>
2016-07-29 20:11:03 +0530

    quick sort Coursera number of comparisons -1

--- Sorts/QuickSort-opt.js
+++ Sorts/QuickSort-opt.js
@@ -1,31 +1,38 @@
 var readArray = [];
 var comparisonCount = 0;
-var cc = 0 ; 
+var cc = 0;
 var fs = require('fs');
 fs.readFile('./QuickSort.txt', 'utf-8', (err, data) => {
 	if (err) throw err;
 	readArray = data.split("\r\n").map(Number);
-	quickSort(readArray, 0, readArray.length);
+	//NOTE: Initial wrong condition readArray.length
+	quickSort(readArray, 0, readArray.length - 1);
 	console.log(readArray.toString());
-	console.log("Total Count"+ comparisonCount);
-	console.log("Total Count"+ cc);
+	console.log("Total Count" + comparisonCount);
+	console.log("Total Count" + cc);
 });
 
 function quickSort(arr, lo, hi) {
-	if (hi - lo < 2) {
+	//NOTE : Intial wrong condition hi - lo
+	if ((hi - lo +1) < 2) {
 		return;
 	}
 	var partitionIndex = partition(arr, lo, hi);
+	//console.log(partitionIndex);
+	//console.log(arr);
 	quickSort(arr, lo, partitionIndex - 1);
-	comparisonCount += partitionIndex - lo - 1 ;//(partitionIndex - 1) - (lo) + 1
+	comparisonCount += partitionIndex - lo - 1; //(partitionIndex - 1) - (lo) + 1
 	quickSort(arr, partitionIndex + 1, hi);
-	comparisonCount += hi - partitionIndex + 1 ;
+	comparisonCount += hi - partitionIndex + 1;
 	//console.log(arr);
 
 }
 
 function partition(arr, lo, hi) {
-	cc += hi-lo;
+	cc += hi - lo;
+	/*var t = arr[hi];
+	arr[hi] =  arr[lo];
+	arr[lo] = t;*/
 	var partitionValue = arr[lo];
 	var i = lo + 1;
 	var j = lo + 1;
@@ -33,10 +40,11 @@ function partition(arr, lo, hi) {
 	while (i <= hi) {
 		//cc++;
 		if (arr[i] < partitionValue) {
-			temp = arr[j]
+			temp = arr[j];
 			arr[j] = arr[i];
 			arr[i] = temp;
 			j++;
+			//console.log(arr);
 		}
 		i++;
 	}
@@ -44,5 +52,5 @@ function partition(arr, lo, hi) {
 	arr[lo] = arr[j - 1];
 	arr[j - 1] = temp;
 
-	return j-1;
+	return j - 1;
 }
