147ffbcad762536fa548efda6fcda50e03c37b94
Mayank Jain <mayank13@gmail.com>
2016-07-29 17:17:01 +0530

    Quick Sort bug fix

--- Sorts/QuickSort-opt.js
+++ Sorts/QuickSort-opt.js
@@ -1,10 +1,14 @@
 var readArray = [];
+var comparisonCount = 0;
+var cc = 0 ; 
 var fs = require('fs');
-fs.readFile('./quickSort.txt', 'utf-8', (err, data) => {
+fs.readFile('./QuickSort.txt', 'utf-8', (err, data) => {
 	if (err) throw err;
 	readArray = data.split("\r\n").map(Number);
-	quickSort(readArray, 0 , readArray.length);
-	console.log(readArray);
+	quickSort(readArray, 0, readArray.length);
+	console.log(readArray.toString());
+	console.log("Total Count"+ comparisonCount);
+	console.log("Total Count"+ cc);
 });
 
 function quickSort(arr, lo, hi) {
@@ -13,30 +17,32 @@ function quickSort(arr, lo, hi) {
 	}
 	var partitionIndex = partition(arr, lo, hi);
 	quickSort(arr, lo, partitionIndex - 1);
+	comparisonCount += partitionIndex - lo - 1 ;//(partitionIndex - 1) - (lo) + 1
 	quickSort(arr, partitionIndex + 1, hi);
+	comparisonCount += hi - partitionIndex + 1 ;
 	//console.log(arr);
 
 }
 
 function partition(arr, lo, hi) {
+	cc += hi-lo;
 	var partitionValue = arr[lo];
 	var i = lo + 1;
 	var j = lo + 1;
 	var temp;
 	while (i <= hi) {
+		//cc++;
 		if (arr[i] < partitionValue) {
 			temp = arr[j]
 			arr[j] = arr[i];
 			arr[i] = temp;
 			j++;
-		} 
+		}
 		i++;
 	}
 	temp = arr[lo];
 	arr[lo] = arr[j - 1];
 	arr[j - 1] = temp;
 
-	return j;
+	return j-1;
 }
-
-quickSort([5, 3, 7, 4], 0, 3);
