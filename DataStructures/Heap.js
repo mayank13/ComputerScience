12ffbd1405c4f0062b969c0663586b2bfbd14adb
Mayank Jain <mayank13@gmail.com>
2016-08-05 18:29:22 +0530

    Heap API

---
+++ DataStructures/Heap.js
@@ -0,0 +1,15 @@
+function minHeap(){
+	this.data = [];
+}
+
+minHeap.prototype.minimum = function(){
+	return this.data[1];
+}
+
+minHeap.prototype.insert = function(key){
+
+}
+
+minHeap.prototype.deleteMinimum = function(){
+	
+}
