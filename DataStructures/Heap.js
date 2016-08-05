f836ed775339784c54d4826ee507e39839dd330c
Mayank Jain <mayank13@gmail.com>
2016-08-05 20:16:26 +0530

    Remove minimum element from the Heap

--- DataStructures/Heap.js
+++ DataStructures/Heap.js
@@ -1,14 +1,14 @@
-
 function minHeap() {
 	this.data = [];
-	this.lastPosition = 1;
+	this.lastPosition = 0;
 }
 
 minHeap.prototype.minimum = function() {
 	return this.data[1];
-}
+};
 
 minHeap.prototype.insert = function(key) {
+	this.lastPosition++;
 	this.data[this.lastPosition] = key;
 	var temp;
 	var currentPosition = this.lastPosition;
@@ -18,15 +18,37 @@ minHeap.prototype.insert = function(key) {
 		this.data[parentPosition] = this.data[currentPosition];
 		this.data[currentPosition] = temp;
 		currentPosition = parentPosition;
-		parentPosition = Math.floor(parentPosition/2);
+		parentPosition = Math.floor(parentPosition / 2);
 	}
-	this.lastPosition++;
 
-}
+
+};
 
 minHeap.prototype.deleteMinimum = function() {
+	var currentPosition = 1;
+	var temp = this.data[this.lastPosition];
+	this.data[this.lastPosition] = this.data[currentPosition];
+	this.data[currentPosition] = temp;
+	this.data.splice(this.lastPosition, 1);
+	console.log(this.data);
+		//Bubble down the newly inserted node
+	while ((this.data[currentPosition * 2] && (this.data[currentPosition] > this.data[currentPosition * 2])) || (this.data[currentPosition * 2 + 1] && (this.data[currentPosition] > this.data[currentPosition * 2 + 1]))) {
+		//NOTE : Mistake in conditional logic. The current node should be swapped with the minimum element of the two children of the binary heap
+		if (this.data[currentPosition*2] < this.data[currentPosition * 2 +1]) {
+			temp = this.data[currentPosition];
+			this.data[currentPosition] = this.data[currentPosition * 2];
+			this.data[currentPosition * 2] = temp;
+			currentPosition = currentPosition * 2;
+		} else {
+			temp = this.data[currentPosition];
+			this.data[currentPosition] = this.data[currentPosition * 2 + 1];
+			this.data[currentPosition * 2 + 1] = temp;
+			currentPosition = currentPosition * 2 + 1;
+		}
+	}
+	this.lastPosition--;
 
-}
+};
 
 //Test heap 
 
@@ -40,4 +62,11 @@ console.log(heap.data);
 heap.insert(4);
 console.log(heap.data);
 heap.insert(3);
+console.log(heap.data);
+
+heap.deleteMinimum();
+console.log(heap.data);
+
+
+heap.deleteMinimum();
 console.log(heap.data);
